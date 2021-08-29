f3bad5364af6a6fc660ecd13f8ba934f1edc6a4b
Sunayana-hub <69916321+Sunayana-hub@users.noreply.github.com>
2021-08-29 14:40:09 +0530

    Update push.sh

--- jenkins/push.sh
+++ jenkins/push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker push sona09/tomcat:latest1
-docker -H ssh://jenkins@52.66.44.43 run -d -p 8086:8086 sona09/tomcat:latest1
+docker run -p 192.168.1.100:8080:8086 run -d sona09/tomcat:latest1
